d3a398f1f1df245bddbcd7b36e4ac9f082546b3e
juzdalua <bearwcw@naver.com>
2024-08-02 00:43:40 +0900

    Update DeadLock Profiler with using TLS stack

--- ServerCore/DeadLockProfiler.cpp
+++ ServerCore/DeadLockProfiler.cpp
@@ -24,7 +24,7 @@ void DeadLockProfiler::PushLock(const char* name)
 	// 잡고 있는 락이 있었다면
 	if (LLockStack.empty() == false) {
 		//기존에 발견되지 않은 케이스라면 데드락 여부 다시 확인한다.
-		const int32 prevId = _lockStack.top();
+		const int32 prevId = LLockStack.top();
 		if (lockId != prevId) {
 			set<int32>& history = _lockHistory[prevId];
 			if (history.find(lockId) == history.end()) {
