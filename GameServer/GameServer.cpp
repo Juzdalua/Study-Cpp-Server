e3da0bac4cada9604c8f750e80a28f09d2082169
juzdalua <bearwcw@naver.com>
2024-08-03 01:01:58 +0900

    Add RefCounting.h with Shared PTR

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -3,82 +3,64 @@
 #include "CoreMacro.h"
 #include "ThreadManager.h"
 #include <iostream>
+#include "RefCounting.h"
 
-#include "AccountManager.h"
-#include "PlayerManager.h"
-
-class TestLock {
-	USE_LOCK;
-
+class Wraith: public RefCountable {
 public:
-	int32 TestRead() {
-		READ_LOCK;
-		if (_queue.empty())
-			return -1;
-
-		return _queue.front();
-	}
-
-	void TestPush() {
-		WRITE_LOCK;
-		_queue.push(rand() % 10);
-	}
-	void TestPop() {
-		WRITE_LOCK;
-		if (_queue.empty() == false)
-			_queue.pop();
-	}
-
-private:
-	queue<int32> _queue;
+	int _hp = 150;
+	int _posX = 0;
+	int _posY = 0;
 };
 
-TestLock testLock;
+using WraithRef = TSharedPtr<Wraith>;
 
-void ThreadWrite() {
-	while (true) {
-		testLock.TestPush();
-		this_thread::sleep_for(1ms);
-		testLock.TestPop();
-	}
-}
+class Missile : public RefCountable {
+public:
 
-void ThreadRead() {
-	while (true) {
-		int32 value = testLock.TestRead();
-		cout << value << endl;
-		this_thread::sleep_for(1ms);
+	void SetTarget(WraithRef target) {
+		_target = target;
 	}
-}
+
+	bool Update() {
+		if (_target == nullptr)
+			return true;
+
+		int posX = _target->_posX;
+		int posY = _target->_posY;
+
+		// TODO: 쫓아간다
+
+		if (_target->_hp == 0) {
+			_target = nullptr;
+			return true;
+		}
+		return false;
+	}
+
+	WraithRef _target = nullptr;
+};
+
+using MissileRef = TSharedPtr<Missile>;
 
 int main()
 {
-	for (int32 i = 0; i < 2; i++) {
-		GThreadManager->Launch(ThreadWrite);
-	}
+	WraithRef wraith(new Wraith());
+	wraith->ReleaseRef();
+	MissileRef missile(new Missile());
+	missile->ReleaseRef();
+	missile->SetTarget(wraith);
 
-	for (int32 i = 0; i < 5; i++) {
-		GThreadManager->Launch(ThreadRead);
-	}
-	
+	// 레이스가 피격 당함
+	wraith->_hp = 0;
+	wraith = nullptr; // WraithRef(nullptr)
 
-	/*GThreadManager->Launch([=]() {
-		while (true) {
-			cout << "PlayerThenAccount" << endl;
-			GPlayerManager.PlayerThenAccount();
-			this_thread::sleep_for(100ms);
+	while (true) {
+		if (missile) {
+			if (missile->Update()) {
+				missile->ReleaseRef();
+				missile = nullptr;
+			}
 		}
-	});
-
-	GThreadManager->Launch([=]() {
-		while (true) {
-			cout << "AccountThenPlayer" << endl;
-			GAccountManager.AccountThenPlayerLock();
-			this_thread::sleep_for(100ms);
-		}
-		});*/
-
-	GThreadManager->Join();
-
-
+	}
+	missile = nullptr;
 }
