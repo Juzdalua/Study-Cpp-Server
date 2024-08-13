46acddba70857b41efe6b969f5b1d16bf9868bfe
juzdalua <bearwcw@naver.com>
2024-08-13 21:50:34 +0900

    Add IOCP AcceptEx

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -3,20 +3,27 @@
 #include "ThreadManager.h"
 #include "SocketUtils.h"
 #include <iostream>
+#include "Listener.h"
 
 int main()
 {
-	SOCKET socket = SocketUtils::CreateSocket();
+	/*SOCKET socket = SocketUtils::CreateSocket();
 	SocketUtils::BindAnyAddress(socket, 7777);
 	SocketUtils::Listen(socket);
-
 	SOCKET clientSocket = accept(socket, nullptr, nullptr);
+	cout << "Client Connected!" << endl;*/
 
-	cout << "Client Connected!" << endl;
+	Listener listener;
+	listener.StartAccept(NetAddress(L"127.0.0.1", 7777));
 
-	while (true)
+	for (int32 i = 0; i < 5; i++)
 	{
-
+		GThreadManager->Launch([=]() {
+			while (true)
+			{
+				GIocpCore.Dispatch();
+			}
+			});
 	}
 
 	GThreadManager->Join();
