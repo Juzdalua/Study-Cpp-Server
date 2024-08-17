418cdd22407e83694f76d9714779800bcedaa572
juzdalua <bearwcw@naver.com>
2024-08-18 00:00:59 +0900

    Update SendBuffer with Chunk & Pooling

--- ServerCore/CoreGlobal.cpp
+++ ServerCore/CoreGlobal.cpp
@@ -4,16 +4,19 @@
 #include "DeadLockProfiler.h"
 #include "Memory.h"
 #include "SocketUtils.h"
+#include "SendBuffer.h"
 
 ThreadManager* GThreadManager = nullptr;
 DeadLockProfiler* GDeadLockProfiler = nullptr;
 Memory* GMemory = nullptr;
 CoreGlobal GCoreGlobal;
+SendBufferManager* GSendBufferManager = nullptr;
 
 CoreGlobal::CoreGlobal() {
 	GThreadManager = new ThreadManager;
 	GMemory = new Memory();
 	GDeadLockProfiler = new DeadLockProfiler;
+	GSendBufferManager = new SendBufferManager();
 	SocketUtils::Init();
 }
 
@@ -21,5 +24,6 @@ CoreGlobal::~CoreGlobal() {
 	delete GThreadManager;
 	delete GMemory;
 	delete GDeadLockProfiler;
+	delete GSendBufferManager;
 	SocketUtils::Clear();
 } 
--- ServerCore/CoreGlobal.h
+++ ServerCore/CoreGlobal.h
@@ -1,8 +1,9 @@
 #pragma once
 
-extern class ThreadManager* GThreadManager;
-extern class DeadLockProfiler* GDeadLockProfiler;
-extern class Memory* GMemory;
+extern class ThreadManager*			GThreadManager;
+extern class DeadLockProfiler*		GDeadLockProfiler;
+extern class Memory*				GMemory;
+extern class SendBufferManager*		GSendBufferManager;
 
 class CoreGlobal
 {
